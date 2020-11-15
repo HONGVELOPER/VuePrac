bbbeb50f12cb2785cc3397c6f9048e8d32beeafe
yjcoder <hyjinn1003@naver.com>
2020-11-15 16:45:02 +0900

    todo 예제 연습

--- todo/src/App.vue
+++ todo/src/App.vue
@@ -4,10 +4,18 @@
     <input 
       type="text" 
       class="w-100 p-2" 
-      placeholder=  "Type todo"
+      placeholder= "what i have to do today ? "
       @keyup.enter="addTodo">
     <hr>
-    <Todo v-for="todo in todos" :key="todo.id" :todo="todo" />
+    <Todo 
+      v-for="todo in todos" 
+      :key="todo.id" 
+      :todo="todo"
+      @toggle-checkbox="toggleCheckbox"
+      @click-delete="deleteTodo" 
+    />
+    <!-- :todo="todo" 는 todos안에 배열 1개를 todo로 설정한것과 Todo.vue 의 
+    props 에 있는 todo와 데이터를 바인딩 시켜주는 역할. -->
   </div>
 </template>
 
@@ -22,18 +30,30 @@ export default {
       todoText: '',
       todos: [
         { id: 1, text: 'how to use github', checked: false},
-        { id: 2, text: 'study of markdown language', checked: false},
+        { id: 2, text: 'learning of markdown language', checked: false},
       ]
     }
   },
   methods: {
+    deleteTodo(id) {
+      const index = this.todos.findIndex(todo => {
+        return todo.id === id;
+      });
+      this.todos.splice(index, 1);
+    },
     addTodo(e) {
       this.todos.push({
         id: Math.random(),
         text: e.target.value,
         checked: false     
       });
-      this.todoText= '';
+      this.todoText='';
+    },
+    toggleCheckbox({id, checked}) {
+      const index = this.todos.findIndex(todo => {
+        return todo.id === id;
+      });
+      this.todos[index].checked = checked;
     }
   }
 }
--- todo/src/components/Todo.vue
+++ todo/src/components/Todo.vue
@@ -1,7 +1,23 @@
 <template>
-  <div>
-      <input type="checkbox" :checked="todo.checked"> 
-      <span class="ml-3">{{ todo.text }}</span> 
+  <div class="mb-2 d-flex">
+      <div>
+          <input type="checkbox" :checked="todo.checked"
+          @change="toggleCheckbox"> 
+      </div>
+
+      <span 
+        class="ml-3 flex-grow-1"
+        :class="todo.checked ? 'text-muted' : '' "
+        :style="todo.checked ? 'text-decoration: line-through' : ''"
+      >
+      <!-- "text-muted"  는 글씨를 회색으로 바꿔주는 역할
+        "text-decoration: line-through" 는 글씨위에 가로줄을 그어주는 역할 -->
+          {{ todo.text }}
+      </span> 
+      <button 
+        class="btn btn-danger btn-sm"
+        @click="clickDelete"
+      >Delete</button>
   </div>
 </template>
 
@@ -12,10 +28,21 @@ export default {
             type: Object,
             required: true
         }
+    },
+    methods: {
+        toggleCheckbox(e) {
+            this.$emit('toggle-checkbox', {
+                id: this.todo.id,
+                checked: e.target.checked 
+            })
+        },
+        clickDelete() {
+            this.$emit('click-delete', this.todo.id)
+        }
     }
 }
 </script>
 
 <style>
 
-</style>
+</style> 
